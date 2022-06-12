86ed8b8c5cd1396e84dd38ab58d64f9553e1dc54
Ioannis Valasakis <code@wizofe.uk>
2022-06-12 12:08:12 +0100

    Walk through ABIDE subfolders and create job files

---
+++ degree_centrality/walk_and_dc.py
@@ -0,0 +1,26 @@
+import os
+import glob
+from pathlib import Path
+
+
+## Quick script to generate an sge.index with names of files
+## to be processed on the NAN server (SUN Grid)
+## ---
+## Copyright 2022 Ioannis Valasakis <code@wizofe.uk>
+## under the GNU GPL v3.0+
+
+cwd = str(Path().resolve())
+
+# grab each site and save it inside a list
+sites = [f for f in os.listdir(cwd) if (not f.startswith('.') and os.path.isdir(f))]
+
+# for every site transverse into derivatives/fmriprep/sub-xxxx
+# get the *MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz
+for site in sites:
+    bolds = glob.glob(f"{cwd}/{site}/**/*MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz", recursive=True)
+    for f in bolds:
+        with open("sge.index", "a") as sge:
+            sge.write(f+'\n')
+
+# write each resulting filename to an sge.index file )one line per file
+
